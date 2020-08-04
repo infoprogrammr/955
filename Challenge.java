e4f68213bf16da9c8ef54729e7623946b85e4d28
candidate <candidate@programmr.com>
2020-08-04 11:06:58 +0000

    sub

--- Challenge.java
+++ Challenge.java
@@ -6,18 +6,22 @@ public class Challenge {
 	public static void main(String args[]) { 
 		Scanner scanner = new Scanner(System.in); 
 		System.out.println("What is your name: "); 
-		String name = scanner.next();
+        String name = scanner.next();
+        	System.out.println("age:"); 
+		String age = scanner.next();
 		String echo = ""; 
 		///{ 
 	 
 		//start your coding here  
      
-        //echo = "Hello "+name;
+        echo = "Hello "+name;
+    
            
 
 		///}   
  
                 System.out.println(echo);
+                 System.out.println("age:"+age);
                 scanner.close();
 	} 
 } 
